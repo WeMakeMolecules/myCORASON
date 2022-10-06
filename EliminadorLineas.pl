f34cdf77f9f69aee8a7f86ca70264de72af83776
Pablo Cruz-Morales <68575424+pablo-genomes-to-vials-cruz@users.noreply.github.com>
2022-10-06 12:06:08 +0200

    Delete EliminadorLineas.pl

--- EliminadorLineas.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/local/bin/perl -w
-#latest version modified by Pablo Cruz-Morales June 2020
-use experimental 'smartmatch';
-use lib './';
-use globals2;
-my $directory =  "$dir2/$NAME2/CONCATENADOS";
-`perl -I . -pi -e "s/^\n//" $directory/*`;            ####delete double lines                    
-
