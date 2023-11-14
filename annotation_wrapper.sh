f5fea5e3ad09acecc8debd218640f34742b5c7da
Pablo Cruz-Morales <68575424+pablo-genomes-to-vials-cruz@users.noreply.github.com>
2023-11-14 11:07:35 +0100

    Update annotation_wrapper.sh

--- annotation_wrapper.sh
+++ annotation_wrapper.sh
@@ -1,5 +1,5 @@
 #wrapper for prodigal gff/raw_prot and antismash gbk to CORASON
 LABEL=$(echo "$1" | sed 's/.fna//')
-prodigal -i $LABEL -a $LABEL.prot_raw -f gff -o $LABEL.gff
+prodigal -i $LABEL.fna -a $LABEL.prot_raw -f gff -o $LABEL.gff
 run_antismash $LABEL.fna $LABEL --genefinding-gff3 /input/$LABEL.gff --taxon bacteria --fullhmmer --cc-mibig --cb-knownclusters
 perl GFF_GBK_to_CORASON.pl $LABEL.prot_raw $LABEL.gbk $LABEL.gff
