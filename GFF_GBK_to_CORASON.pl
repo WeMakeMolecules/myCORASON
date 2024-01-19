c693cc411288fef8b0b78b478faa34d6e3d16108
Pablo Cruz-Morales <68575424+pablo-genomes-to-vials-cruz@users.noreply.github.com>
2024-01-19 18:59:40 +0100

    Update GFF_GBK_to_CORASON.pl

--- GFF_GBK_to_CORASON.pl
+++ GFF_GBK_to_CORASON.pl
@@ -1,19 +1,19 @@
 #creates a unique ID for this run
 $rand1=int(rand(1000000));
 $rand2=int(rand(1000000));
-print "###########################################################\n";
+print "#########################################################\n";
 print "## GBK GFF TO CORASON BACTERIAL			        ##\n";
 print "## A script to convert PRODIGAL proteome and GFF         ##\n";
-print "## and antismash v7 GBK in CORASON format                ##\n";
-print "## by Pablo Cruz-Morales at DTU Biosustain 	        ##\n";
-print "## pcruzm\@biosustain.dtu.dk                	        ##\n";
-print "## may 2023				                ##\n";
-print "## Inputs: a GGF file with the PRODIGAL gene calling     ##\n";
-print "##        a prot_raw file from prodigal (-a flag)        ##\n";
-print "##        a GBK file with the  ANTISMASH 6 annotation    ##\n";
-print "##perl GFF_GBK_to_CORASON file.prot_raw file.gbk file.gff##\n";
+print "## and antismash v7 GBK (docker version) in CORASON format##\n";
+print "## by Pablo Cruz-Morales at DTU Biosustain 	       ##\n";
+print "## pcruzm\@biosustain.dtu.dk                	       ##\n";
+print "## Jan 2024				               ##\n";
+print "## Inputs: a GGF file with the PRODIGAL gene calling    ##\n";
+print "##        a prot_raw file from prodigal (-a flag)       ##\n";
+print "##        a GBK file with the  ANTISMASH 6 annotation   ##\n";
+print "## usage: perl GFF_GBK_to_CORASON file.prot_raw file.gbk file.gff   ##\n";
 print "##run id : = $rand1.$rand2                               ##\n";
-print "###########################################################\n";
+print "#########################################################\n";
 $cont="0";
 #Checking that the inputs are correct and providing instruction#
 if ($ARGV[0]=~/.+.prot_raw/){
@@ -28,7 +28,7 @@ else {
 	print "Error\: did you entered a gbk file?\nusage: perl GFF_GBK_to_FUNGISON file.gff file.gbk\n ";
 	}
 #storing the file name for the outputs
-open FILE, $ARGV[0] or die "I cant open the augustus GFF3 file\n";
+open FILE, $ARGV[0] or die "I cant open the raw protein file\n";
 $file_name_raw="$ARGV[0]";
 $file_name_raw=~/(.+)(.prot_raw)/;
 $file_name="$1";
@@ -44,6 +44,7 @@ while ($line=<FILE>){
 	}
 	else {
 	chomp $line;
+	$line=~s/\n//g;
 	$line=~s/\*//g;
 	print  AMINO "$line";
 	}
@@ -57,6 +58,7 @@ system "tr <amino_acids_$rand1.$rand2 '\t' '\n'|sed 's/fig|/>fig|/' |grep '^\$' 
 
 
 #Creating a functions table using a GBK file from antismash with fullhmmer option on 
+$id=0;
 open GEBEKA,  ">functions_table_$rand1.$rand2";
 system  "grep \-E \'/ID\=".+"\|\/description\' $ARGV[1]\| tr \'\\n\' \' \'\|sed \'s/\\/ID\/\\n/g\'\|sed \'s\/  \/\/g\' \> raw_grepped_$rand1.$rand2";
 open FILEGREP, "raw_grepped_$rand1.$rand2";
@@ -64,9 +66,9 @@ while($line=<FILEGREP>){
 	if ($line=~/\s+\n/){$dummy="1";next;}
 
 	if ($line=~/="\d+\_\d+"\/description\=\".+/){
-	
+$id++;
 	$line=~/(="\d+\_)(\d+)("\/description\=\")(.+)/;
-	$id="$2";
+#	$id="$2";
 	$annotation="$4";
 	$annotation=~s/\/description\=\"//g;
 	$annotation=~s/Condensation domain"/C-/g;
@@ -80,7 +82,7 @@ while($line=<FILEGREP>){
 	$annotation=~s/Acyl transferase domain"/AT-/g;
 	$annotation=~s/Ketoacyl-synthetase C-terminal extension"Beta-ketoacyl synthase, C-terminal domain"Beta-ketoacyl synthase, N-terminal domain"/KS-/g;
 	$annotation=~s/Beta-ketoacyl synthase, N-terminal domain"Beta-ketoacyl synthase, C-terminal domain"/KS-/g;
-  $annotation=~s/N-terminal domain"Beta-ketoacyl synthase, C-terminal domain"Beta-ketoacyl synthase, N-terminal domain"/KS-/g;
+        $annotation=~s/N-terminal domain"Beta-ketoacyl synthase, C-terminal domain"Beta-ketoacyl synthase, N-terminal domain"/KS-/g;
 	$annotation=~s/KS-Ketoacyl-synthetase C-terminal extension"/KS-/g;
 	$annotation=~s/Zinc-binding dehydrogenase"/ER-/g;
 	$annotation=~s/Male sterility protein"/Acyl-loading-/g;
@@ -102,7 +104,7 @@ while($line=<FILEGREP>){
 	}
 	else {
 	$line=~/(="\d+\_)(\d+)/;	
-	$id="$2";
+	$id++;
 	$annotation="unknown function";
 	$id=~s/\=\"//;
 	$id=~s/"//;
@@ -114,6 +116,7 @@ close FILEGREP;
 close GEBEKA;
 #getting a list of coordinates gene numbers and strand
 open COORDINATES, "$ARGV[2]" or die "I cant open the prodigal GFF3 file\n";
+
 #making the table (txt) file
 print TABLE "contig_id	feature_id	type	location	start	stop	strand	function	locus_tag	figfam	species	nucleotide_sequence	amino_acid	sequence_accession\n";
 while ($line=<COORDINATES>){
@@ -151,6 +154,7 @@ while ($line=<COORDINATES>){
 			$funcid="$1";
 			$function="$2";
 			$match="fig\|$rand1.$rand2.peg.$funcid";
+
 			if ($match eq $gene){
 	#checking the orientation of the gene, if negative then flipping the numbers, this makes arrrows go in the right direction
 			if ($strand=~/\+/){
@@ -178,10 +182,10 @@ while ($line=<COORDINATES>){
 	$cont++;
 	}
 }
-print TABLE "$contig\t$gene\t$type\t$location\t$start\t$end\t$strand\t$function\t$locustag\t$figfam\t$species\t$nucleotide\t$aminoacid\t$accession\n";
+#print TABLE "$contig\t$gene\t$type\t$location\t$start\t$end\t$strand\t$function\t$locustag\t$figfam\t$species\t$nucleotide\t$aminoacid\t$accession\n";
 close COORDINATES;
 close AMINOSEQ;
 close COORDINATES;
 close TABLE;
-system "rm raw_grepped_$rand1.$rand2 amino_acids_$rand1.$rand2 functions_table_$rand1.$rand2";
+system "rm  raw_grepped_$rand1.$rand2 amino_acids_$rand1.$rand2 functions_table_$rand1.$rand2";
 ;;
